a32a6ee3c8053ea2090a2bf8f199cab858e4d1a3
xemon <xemon@toshi>
2023-03-31 20:59:36 +0000

    New updated version with ability to stop overcharge and undercharge the callsign can be changed

--- takis-o-robakis.cpp
+++ takis-o-robakis.cpp
@@ -5,14 +5,37 @@ using namespace std;
 class robot {
 	public:
 	
-	robot(int in_callsign,int in_battery);
+	robot(string in_callsign,int in_battery, int in_overcharge);
 	
-	void setbattery(int sbattery){
-		battery=sbattery;
+	void moveforward();
+	void moveleft();
+	void moveright();
+	void charge();
+	
+	void setcallsign(string in_callsign){
+		callsign=in_callsign;
+	};
+	
+	int getoverchargelimit(){
+	return overcharge_limit;
+	}
+	
+	string getcallsign(){
+		return callsign;
+	}
+	
+	void addbattery(int sbattery){
+		int tempbattery;
+		tempbattery = battery + sbattery;
+		if (tempbattery < overcharge_limit){
+		battery=battery+sbattery;
+	}else{
+		cout << "Cannot charge the battery above overcharge limit";
+		}
 	};
 	
 	void rembattery(int rbattery){
-		battery=rbattery-battery;
+		battery=battery-rbattery;
 	};
 	
 	int getbattery(){
@@ -23,38 +46,156 @@ class robot {
 	
 	string callsign;
 	int battery;
-
+	int overcharge_limit;
 	
 	
 };
 
-robot::robot(string in_callsign,int battery){
+robot::robot(string in_callsign,int in_battery,int in_overcharge){
 callsign=in_callsign;
 battery=in_battery;
+overcharge_limit=in_overcharge;
 };
 
+void robot::moveforward(){
+rembattery(100);
+};
+
+void robot::moveright(){
+rembattery(50);
+};
+
+void robot::moveleft(){
+rembattery(50);
+}
+
+//void robot::charge(){
+//addbattery(300);
+//}
+	
 
 int main () {
+	
+	string new_callsign;
+	string PROMPT_MSG;
+	char PROMPT=1;
+	
+	//default callsign, battery level & battery overcharge limit
+	robot takis("C8fb",500,1200);
 
-int PROMPT=0
+	while ( PROMPT != 9 ){
+		
+	cout << "\n\n\n\n";
+	cout << "======================================================\n";
+	cout << "================Takis-O-Robotakis=====================\n";
+	cout << "======================================================\n";
 
-robot takis(C8fb,500);
+	cout << "\n\n\n\n";
 
-while ( PROMPT == 0 ){
+	cout << "Press 1 to print his callsign\n";
+	cout << "Press 2 to move forward\n";
+	cout << "Press 3 to turn right\n";
+	cout << "Press 4 to turn left\n";
+	cout << "Press 5 charge takis 300 battery points\n";
+	cout << "Press 6 to set his callsign\n\n";
 
-cout << "======================================================\n";
-cout << "================Takis-O-Robotakis=====================\n";
+	cout << "To exit press 9\n\n";
 
-cout << "Press 1 to say hello\n";
-cout << "Press 2 to turn left\n";
-cout << "Press 3 to turn right\n";
-cout << "Press 4 to move forward\n";
+	cout << "Takis current battery level:" << takis.getbattery(); 
+	
+	cout << "\n\n";
+	
+	
 
-cout << "Takis current battery level:";
+	cout << PROMPT_MSG;
+	PROMPT_MSG=" ";
+	cout << "\n\n";
+	
+	cout << "Select one of the following actions:";
+	cin >> PROMPT;
 
-cout << "Hello My Friend,My callsign is";;
+	switch (PROMPT){
+		case 1:
+			PROMPT_MSG="Hello My Friend,My callsign is:" + takis.getcallsign() + "\n";
+			cout << takis.getcallsign();
+			break;
+			
+		case 2:
+			//does not allow the battery to get below 0
+			if (takis.getbattery() > 0){
+				takis.moveforward();
+				PROMPT_MSG="Takis moved forward\n";
+			}else{
+				PROMPT_MSG="Takis cannot move! Battery level dropped to zero!";
+			}
+			
+			break;
+			
+		case 3:
+			//does not allow the battery to get below 0
+			if (takis.getbattery() > 0){
+			takis.moveright();
+			PROMPT_MSG="Takis moved right\n";
+			}else{
+			 PROMPT_MSG="Takis cannot move! Battery level dropped to zero!";
+			}
+			break;
+			
+		case 4:
+			//does not allow the battery to get below 0
+			if (takis.getbattery() > 0){
+			takis.moveleft();
+			PROMPT_MSG="Takis moved left\n";
 
-cin >> PROMPT;
+			}else{
+			 PROMPT_MSG="Takis cannot move battery level dropped to zero!";
+			}
+
+			break;
+			
+		case 5:
+		
+			//takis overcharge protection
+			int temp_battery;
+			int temp_overcharge_limit;
+			int charge_energy;
+			int total_energy;
+			
+			charge_energy=300;
+			
+			temp_battery=takis.getbattery();
+			charge_energy=300;
+			//calculates total energy so limit wont ever be passed
+			total_energy = charge_energy + temp_battery;
+			
+			//retrieves takis energy limit
+			temp_overcharge_limit=takis.getoverchargelimit();
+			
+			//only allow value below the overcharge limit
+			if ( total_energy < temp_overcharge_limit){
+				takis.addbattery(charge_energy);
+				PROMPT_MSG="Takis got charged 300 energy points\n";
+			} else{
+				PROMPT_MSG="Cannot charge takis byond his limit! If overcharged he is going to blow-up!\n";
+			}
+				break;
+				
+		case 6:
+
+			cout << "Write takis new callsign:";
+			cin  >> new_callsign;
+			takis.setcallsign(new_callsign);
+			break;
+		
+		default:
+		
+		PROMPT_MSG="Wrong option";
+		
+	}
+
+
+
+
 };
 	
 	
